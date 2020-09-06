8e1d74f328bcbcfcef96878a4bde870876cb11f5
Ellingduck <markus.elling@live.dk>
2020-09-06 16:06:57 +0200

    Authentication setup
    Authentication has been setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Auth0Provider } from "@auth0/auth0-react";
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-mj.eu.auth0.com"
+    clientId="Iuw2MmIL0qhSo8riycKJn2Fp7xw8SPRk"
+    redirectUri={window.location.origin}
+  >
+  <App />
+  </Auth0Provider>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
